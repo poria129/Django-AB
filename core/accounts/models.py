642cd55b01098da79f5cea7f5f1fceeb59a24cd2
poria129 <89725449+poria129@users.noreply.github.com>
2022-12-15 00:51:57 +0330

    Update models.py

--- core/accounts/models.py
+++ core/accounts/models.py
@@ -31,7 +31,12 @@ class UserManager(BaseUserManager):
         extra_fields.setdefault('is_staff', True)
         extra_fields.setdefault('is_superuser', True)
         extra_fields.setdefault('is_active', True)
-        pass
+    
+        if extra_fields.get('is_staff') is not True:
+            raise ValueError(_('Superuser must have is_staff=True.'))
+        if extra_fields.get('is_superuser') is not True:
+            raise ValueError(_('Superuser must have is_superuser=True.'))
+        return self.create_user(email, password, **extra_fields)
 
 
 # castum model user for our app.
