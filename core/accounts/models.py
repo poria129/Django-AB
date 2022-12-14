8985fc88392da89e32b0f7483e3fdf18b3213fa0
poria129 <89725449+poria129@users.noreply.github.com>
2022-12-14 19:10:55 +0330

    eshtebah model
    eshtebahi model blog ro baraye accounts zade boodam

--- core/accounts/models.py
+++ core/accounts/models.py
@@ -1,26 +1,3 @@
 from django.db import models
 
 # Create your models here.
-class Post(models.Model):
-    '''
-    This is a class to define post for blog app.
-    '''
-    image = models.ImageField(null=True, blank=True)
-    author = models.ForeignKey(User,on_delete=models.CASCADE)
-    title = models.CharField(max_length=250)
-    content = models.TextField()
-    category = models.ForeignKey('Category',on_delete=models.SET_NULL,null=True)
-    status = models.BooleanField()
-    
-    created_date = models.DateTimeField(auto_now_add=True)
-    updated_date = models.DateTimeField(auto_now=True)
-    published_date = models.DateTimeField()
-
-    def __str__(self):
-        return self.title
-
-class Category(models.Model):
-    name = models.CharField(max_length=250)
-
-    def __str__(self):
-        return self.name
