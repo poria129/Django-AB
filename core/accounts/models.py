e49f63db1348f60d0cbd4ef5298f6f215a13722a
poria129 <89725449+poria129@users.noreply.github.com>
2022-12-15 21:01:44 +0330

    3.4
    ghesmate 3.4 ro didam va eshtebahat ro dorost kardam

--- core/accounts/models.py
+++ core/accounts/models.py
@@ -17,7 +17,7 @@ class UserManager(BaseUserManager):
         if not email:
             raise ValueError(_('The email must be set'))
         
-        email = self.normilize_email(email)
+        email = self.normalize_email(email)
         user = self.model(email=email, **extra_fields)
         user.set_password(password)
         user.save()
@@ -44,9 +44,9 @@ class User (AbstractBaseUser,PermissionsMixin):
 
     email = models.EmailField(max_length=255,unique=True)
     is_superuser = models.BooleanField(default=False)
-    is_Staff = models.booleanField(default=False)
-    is_active = models.booleanField(default=True)
-    # is_verified = models.booleanField(default=False)
+    is_staff = models.BooleanField(default=False)
+    is_active = models.BooleanField(default=True)
+    # is_verified = models.BooleanField(default=False)
     
     USERNAME_FIELD = 'email'
     REQUIRED_FIELDS = []
--- core/core/settings.py
+++ core/core/settings.py
@@ -39,7 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'accounts',
-    'blog',
+    #'blog',
 ]
 
 MIDDLEWARE = [
@@ -126,3 +126,7 @@ STATIC_URL = '/static/'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# user manager config
+
+AUTH_USER_MODEL = 'accounts.User'
