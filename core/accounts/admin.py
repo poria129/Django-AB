dbc7d40bd3400a547934e855081f86d9ff465bd0
poria129 <89725449+poria129@users.noreply.github.com>
2022-12-18 00:42:30 +0330

    3.7
    ta akhare 3.7 profile ro dorost kardam

--- core/accounts/admin.py
+++ core/accounts/admin.py
@@ -1,3 +1,44 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from .models import User
+
 
 # Register your models here.
+
+class CustomUserAdmin(UserAdmin):
+    model = User
+    list_display = ('email','is_superuser','is_active',)
+    list_filter = ('email','is_superuser','is_active',)
+    searching_fields = ('email',)
+    ordering = ('email',)
+    fieldsets = (
+        ('Authentication',{
+            'fields': (
+                'email', 'password'
+            ),
+        }),
+        ('Permissions',{
+            'fields': (
+                'is_staff', 'is_active', 'is_superuser', 
+            ),
+        }),
+        ('Group permissions',{
+            'fields': (
+                'groups', 'user_permissions',
+            ),
+        }),
+        ('Important dates',{
+            'fields': (
+                'last_login', 
+            ),
+        }),
+    )
+    add_fieldsets = (
+        (None,{
+            'fields': (
+                'email', 'password1', 'password2', 'is_active', 'is_staff', 'is_superuser', 
+            ),
+        }),
+    )
+    
+admin.site.register(User, CustomUserAdmin)
