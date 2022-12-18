2d1992ab1295a9d88a2fdce0624d2bc0e69e7112
poria129 <89725449+poria129@users.noreply.github.com>
2022-12-18 23:11:52 +0330

    3.12
    model ha va profile account va blog taghriban takmil shod va avalin post dar blog sakhte shof

--- core/blog/models.py
+++ core/blog/models.py
@@ -1,12 +1,17 @@
 from django.db import models
+from django.contrib.auth import get_user_model
+
+# get user model objects
+User = get_user_model()
+
 
 # Create your models here.
 class Post(models.Model):
     '''
     This is a class to define post for blog app.
     '''
-    image = models.ImageField(null=True, blank=True)
     author = models.ForeignKey(User,on_delete=models.CASCADE)
+    image = models.ImageField(null=True, blank=True)
     title = models.CharField(max_length=250)
     content = models.TextField()
     category = models.ForeignKey('Category',on_delete=models.SET_NULL,null=True)
