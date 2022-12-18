f38bc77aa1b47a2e51e7bbbe08774637053de2fa
poria129 <89725449+poria129@users.noreply.github.com>
2022-12-18 19:37:39 +0330

    3.9
    ta akhare ghesmat 3.9

--- core/accounts/admin.py
+++ core/accounts/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
-from .models import User
+from .models import User, Profile
 
 
 # Register your models here.
@@ -41,4 +41,5 @@ class CustomUserAdmin(UserAdmin):
         }),
     )
     
+admin.site.register(Profile)
 admin.site.register(User, CustomUserAdmin)
--- core/accounts/models.py
+++ core/accounts/models.py
@@ -1,6 +1,8 @@
 from django.db import models
 from django.contrib.auth.models import (BaseUserManager,AbstractBaseUser,PermissionsMixin)
 from django.utils.translation import ugettext_lazy as _
+from django.db.models.signals import post_save
+from django.dispatch import receiver
 
 # Create your models here.
 
@@ -58,3 +60,25 @@ class User (AbstractBaseUser,PermissionsMixin):
 
     def __str__(self):
         return self.email
+
+class Profile(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    first_name = models.CharField(max_length=250)
+    last_name = models.CharField(max_length=250)
+    image = models.ImageField(blank=True, null=True)
+    description = models.TextField()
+
+    created_date = models.DateTimeField(auto_now_add=True)
+    updated_date = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.user.email
+
+"""
+with reciver and post_save we will create a profile after
+we created a User.
+"""
+@receiver (post_save,sender=User)
+def save_profile(sender, instance, created, **kwargs):
+    if created:
+        Profile.objects.create(user=instance)
