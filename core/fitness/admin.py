87253aff4355124340f5a7e6447ce264ab6a425e
poria129 <89725449+poria129@users.noreply.github.com>
2023-01-20 18:19:38 +0330

    admin listing
    made admin listing

--- core/fitness/admin.py
+++ core/fitness/admin.py
@@ -3,5 +3,10 @@ from .models import Meal, Food
 
 # Register your models here.
 
-admin.site.register(Meal)
+class MealAdmin(admin.ModelAdmin):
+    date_hierarchy = 'created_date'
+    empty_value_display = 'empty'
+    list_display = ('person', 'food', 'meal_calorie', 'created_date')
+
+admin.site.register(Meal,MealAdmin)
 admin.site.register(Food)
